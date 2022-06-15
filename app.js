339f36c4258f9e813bac05ec181e2715f23988d7
ChingG0 <www.karta2428620@gmail.com>
2022-06-15 12:28:53 +0800

    import bootstrap popper

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ const port = 3000
 app.engine('handlebars', exphbs.engine({defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+//setting static files
+app.use(express.static('public'))
 
 //routes setting
 app.get('/',(req,res)=>{
     res.render('index')
 })
 
+//listen express server
 app.listen(port,()=>{
     console.log(`Express is listening on localhost: ${port}`)
 })

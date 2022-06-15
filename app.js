1ce4619aa5e4c29a0d08bb76d66095a9ed13b6ba
ChingG0 <www.karta2428620@gmail.com>
2022-06-15 13:24:04 +0800

    import json

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express')
-const app = express()
-const exphbs = require('express-handlebars')
-const port = 3000
-
+const express = require("express");
+const app = express();
+const exphbs = require("express-handlebars");
+const movieList = require('./movies.json')
+const port = 3000;
 
 //express template engine
 //handlebars
-app.engine('handlebars', exphbs.engine({defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 //setting static files
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 //routes setting
-app.get('/',(req,res)=>{
-    res.render('index')
-})
+app.get("/", (req, res) => {
+  res.render("index", { movies: movieList.results });
+});
 
 //listen express server
-app.listen(port,()=>{
-    console.log(`Express is listening on localhost: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Express is listening on localhost: ${port}`);
+});
